3e4e4e2dba48f8816fa30b225cdcc3c560fdfad0
ricksr <sutirtharej@gmail.com>
2020-11-05 19:04:22 +0530

    landings-steps-done

--- landings/banner_1/src/App.js
+++ landings/banner_1/src/App.js
@@ -5,7 +5,7 @@ import banner_1 from './banner_2.jpeg';
 const App = () => {
   return (
     <div style={{ marginLeft: "1vw", marginRight: "1vw", marginTop: "1vh", marginBottom: "1vh" }}>
-      <a href="http://phnx.surge.sh"> <img src={banner_1} alt="banner_1" style={{ width: "100vw" }} /> </a>
+      <a href="http://axis-merchant-features.surge.sh/"> <img src={banner_1} alt="banner_1" style={{ width: "100vw" }} /> </a>
     </div>
   );
 }
