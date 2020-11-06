35215495ae70569c521c8e734428f5d8c6f406ab
sutirtha rej <sutirtha.rej.fiem.cse16@teamfuture.in>
2020-11-06 18:51:41 +0530

    Update fire.js

--- Front/src/Firebase/fire.js
+++ Front/src/Firebase/fire.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyA3S_JJW0lvdAcUY4yomeG9dVjZMBLTez8",
+    apiKey: "*********************************",
     authDomain: "hitcounter-dded4.firebaseapp.com",
-    databaseURL: "https://hitcounter-dded4.firebaseio.com",
+    databaseURL: "https://XXXXXXXXXXXXXXXXXX.firebaseio.com",
     projectId: "hitcounter-dded4",
     storageBucket: "hitcounter-dded4.appspot.com",
-    messagingSenderId: "9404216278",
-    appId: "1:9404216278:web:f24c26df1e7e88a08daa77",
+    messagingSenderId: "******************",
+    appId: "*****************************",
     measurementId: "G-V51M069G4L"
 };
 // Initialize Firebase
